b71fa0a6d63bfaf6fb07b2bd224c8676a8dbea4e
Tarek Belkahia <tarek.belkahia@gmail.com>
2023-01-05 18:24:00 +0100

    Add snake and cell classes

--- src/main/kotlin/Snake.kt
+++ src/main/kotlin/Snake.kt
@@ -2,4 +2,18 @@ import kotlinx.browser.window
 
 fun main() {
     console.log("Hello from  ${window.navigator.userAgent}")
+
+    val snake = Snake(
+        cells = listOf(Cell(2, 0), Cell(1, 0), Cell(0, 0)),
+        direction = Direction.Right
+    )
+    console.log(snake)
+}
+
+data class Snake(val cells: List<Cell>, val direction: Direction)
+
+data class Cell(val x: Int, val y: Int)
+
+enum class Direction {
+    Up, Down, Left, Right
 }
