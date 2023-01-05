1413de1ea6364e98847b2b38fc135cc35cac969f
Tarek Belkahia <tarek.belkahia@gmail.com>
2023-01-05 18:17:21 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("js") version "1.8.0"
+}
+
+group = "fr.belkahia"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    js {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport {
+                    enabled.set(true)
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/Snake.kt
@@ -0,0 +1,5 @@
+import kotlinx.browser.window
+
+fun main() {
+    console.log("Hello from  ${window.navigator.userAgent}")
+}
