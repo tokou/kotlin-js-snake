400bb3c8c24597c67a75d6d85bed54e265ee728b
Tarek Belkahia <tarek.belkahia@gmail.com>
2023-02-19 14:02:49 +0100

    Setup index and css

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,11 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.8.0")
     testImplementation(kotlin("test"))
 }
 
